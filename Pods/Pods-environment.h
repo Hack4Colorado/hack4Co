f3f69815b63647f4816e02d6fdd49a8f63cef84f
Matt Riddoch <mattr@spiremedia.com>
2014-06-01 10:14:38 -0600

    initial commit

---
+++ Pods/Pods-environment.h
@@ -0,0 +1,26 @@
+
+// To check if a library is compiled with CocoaPods you
+// can use the `COCOAPODS` macro definition which is
+// defined in the xcconfigs so it is available in
+// headers also when they are imported in the client
+// project.
+
+
+// ISMethodSwizzling
+#define COCOAPODS_POD_AVAILABLE_ISMethodSwizzling
+#define COCOAPODS_VERSION_MAJOR_ISMethodSwizzling 0
+#define COCOAPODS_VERSION_MINOR_ISMethodSwizzling 0
+#define COCOAPODS_VERSION_PATCH_ISMethodSwizzling 2
+
+// ISRefreshControl
+#define COCOAPODS_POD_AVAILABLE_ISRefreshControl
+#define COCOAPODS_VERSION_MAJOR_ISRefreshControl 1
+#define COCOAPODS_VERSION_MINOR_ISRefreshControl 4
+#define COCOAPODS_VERSION_PATCH_ISRefreshControl 1
+
+// RQShineLabel
+#define COCOAPODS_POD_AVAILABLE_RQShineLabel
+#define COCOAPODS_VERSION_MAJOR_RQShineLabel 0
+#define COCOAPODS_VERSION_MINOR_RQShineLabel 1
+#define COCOAPODS_VERSION_PATCH_RQShineLabel 1
+
